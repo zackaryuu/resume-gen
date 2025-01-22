610a188f6c147c401709a55dd34840df2a29bd4f
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2025-01-21 22:45:31 -0800

    ref(txt): update wrap format

--- src/regen/presets/txt.py
+++ src/regen/presets/txt.py
@@ -7,6 +7,5 @@ captures = ["output.txt"]
 def run(profile, data):
     build_input_md(data)
     click.echo("Input file created: input.md")
-    
-    os.system(f'pandoc input.md -o output.txt -f markdown -t plain --template="{os.path.join(profile, "template.txt")}"')
+    os.system(f'pandoc input.md -o output.txt -f markdown -t plain --template="{os.path.join(profile, "template.txt")}" --wrap=none')
     click.echo("Output file created: output.txt")
